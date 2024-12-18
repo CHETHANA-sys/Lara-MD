cc4e840c610c01553097e10f094f8c01ec834aa9
CHETHANA-sys <vimurksn07@gmail.com>
2024-12-18 12:52:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "vvYWSlxa#XazQlR1rGBXU_987rHALVw7ZL1jLHLgCuvW5i9XLjk", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "privte",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
